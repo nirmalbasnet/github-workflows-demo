e09cc597ec02c2753fa3e120d0977ed637b212ee
Nirmal Basnet <nirmalbasnet4499@gmail.com>
2024-09-07 21:02:40 +0545

    feat: lint workflow

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,28 +1,28 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import nodeExternals from "webpack-node-externals";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import nodeExternals from 'webpack-node-externals';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const config = {
-  target: "node",
+  target: 'node',
   externals: [nodeExternals()], // Exclude node_modules from being bundled
-  entry: "./main.js",
+  entry: './main.js',
   output: {
-    filename: "bundle.cjs",
-    path: path.resolve(__dirname, "dist"),
-    libraryTarget: "commonjs2",
+    filename: 'bundle.cjs',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
       {
         test: /\.js$/, // Process JavaScript files
-        exclude: /node_modules/,
-      },
-    ],
+        exclude: /node_modules/
+      }
+    ]
   },
-  mode: "production",
+  mode: 'production'
 };
 
 export default config;
